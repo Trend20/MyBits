a4bedfa262ee52be0be193e044999537340f0c01
DevEnock <enockomondi305@gmail.com>
2021-10-13 17:06:58 +0300

    adding the currency component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Currency from './component/Homepage/Currency/Currency';
 import NavBar from './component/NavBar/NavBar';
 
 function App() {
   return (
     <div className="app">
-      <NavBar />
+      
+      <div className="nav-container">
+         <NavBar />
+      </div>
+      <Currency />
     </div>
   );
 }
---
+++ client/src/component/Homepage/Currency/Currency.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Currency.css';
+
+function Currency() {
+    return (
+        <div className="bit-currency">
+            <div className="image">
+                <img src="img/bitcoin.png" alt="bitimage" />
+            </div>
+            <div className="currency-description">
+                 <h1>WHAT IS BITCOIN</h1>
+                 <h3>
+                     New Currency - New Lifestyle!
+                 </h3>
+                 <span></span>
+                 <p>
+                     Bitcoin is a revolutionary invention of the last decade. Its essential meaning is money digitization. It became the first decentralized peer-to-peer payment network for using without any central authority or middlemen. In a nutshell, bitcoin is the money for Internet. Its original purpose is providing all people with universal currency for different operations. Bitcoin can also be described as the most prominent triple entry bookkeeping system in existence.
+                 </p>
+                 <button className="currency-btn">Read more</button>
+            </div>
+        </div>
+    )
+}
+
+export default Currency
