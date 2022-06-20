d63596ee69bdcb32265b797ed5df89b5c7021426
Enock Omondi <enockomondi305@gmail.com>
2022-06-20 16:40:00 +0300

    adding the login page

--- client/src/component/Login/Login.tsx
+++ client/src/component/Login/Login.tsx
@@ -11,25 +11,21 @@ const Login = () =>{
       </LoginText>
       <LoginForm className="login-form">
         <Form>
-          <h1>Welcome to BitUnit</h1>
+          <h1>Welcome to Bitunit</h1>
           <p>Login to access our services</p>
 
-          <div className="form-group">
+          <FormGroup className="form-group">
             <input type="text" placeholder='Username' />
-          </div>
+          </FormGroup>
 
-          <div className="form-group">
+          <FormGroup className="form-group">
             <input type="password" placeholder='Password' />
-          </div>
-          <div className="form-actions">
-            <p>Remember me</p>
-            <p>Forgot password</p>
-          </div>
+          </FormGroup>
 
-          <div className="declarations">
-            <p>By signing up, you agree to our</p>
-              <p>Terms and Conditions & Privacy Policy</p>
-          </div>
+          <Button>Login</Button>
+          <Declarations className="declarations">
+            <p>By signing up, you agree to our Terms and Conditions & Privacy Policy</p>
+          </Declarations>
         </Form>
       </LoginForm>
     </LoginContainer>
@@ -65,5 +61,60 @@ const Form = styled.form`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: #fff;
+  padding: 50px;
+  border-radius: 10px;
+  width: 50%;
+
+  h1{
+    display: flex;
+    opacity: 0.8;
+  }
+
+  p{
+    font-size: 0.9rem;
+    margin-top: 20px;
+  }
+`
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+  width: 100%;
+
+  input{
+    padding: 15px;
+    width: 100%;
+    outline: none;
+    border-radius: 10px;
+    border: 1px solid grey;
+  }
+`
+
+const Declarations = styled.div`
+  display: flex;
+  margin-top: 50px;
+  flex-direction: column;
+  align-items: flex-start;
+
+  p{
+    font-size: 1rem;
+  }
+`
+
+const Button = styled.button`
+  display:: flex;
+  background-color: #fab915;
+  margin-top: 30px;
+  padding: 20px;
+  border: none;
+  outline: none;
+  color: #fff;
+  width: 50%;
+  cursor: pointer;
+  border-radius: 10px;
+  font-weight: 700;
+  font-size: 1rem;
 `
 export default Login;
